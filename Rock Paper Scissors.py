f2e1dcb6f9f4765183fc7e88bca05b7c069b8c81
subiksharaman1 <60492920+subiksharaman1@users.noreply.github.com>
2020-01-31 16:36:57 +0800

    Add files via upload

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,36 @@
+import random
+yourCount = 0
+computerCount = 0
+while True:
+    userin = input("Rock, paper or scissors? ").upper()
+    randNum = random.randint(0,2)   #to generate computer's play
+    myList = ["ROCK", "PAPER", "SCISSORS"]
+    if userin == "ROCK":
+        uservalue = 0
+    elif userin == "PAPER":
+        uservalue = 1
+    elif userin == "SCISSORS":
+        uservalue = 2
+    elif userin == "END":
+        print("GAME OVER.")
+        break
+    else:
+        print("Please enter a valid input. END to stop the game.")
+        continue
+    #3 cases exist:
+    if (uservalue - randNum) == 1 or (uservalue - randNum) == -2:
+        print("Computer played " + myList[randNum] + ", YOU WIN!")
+        yourCount += 1
+    if (uservalue - randNum) == -1 or (uservalue - randNum) == 2:
+        print("Computer played " + myList[randNum] + ", YOU LOSE!")
+        computerCount += 1
+    if uservalue == randNum:
+        print("Computer played " + myList[randNum] + ", it's a draw.")
+    print("Your score: " + str(yourCount) + " Computer's score: " + str(computerCount))
+
+
+
+
+
+
+
